08c1200949fe8bfe274df5e8e2d115989bc6aeba
kelvinvaghasiya2002 <kelvinvaghasiya50@gmail.com>
2024-06-15 20:58:43 +0530

    changed vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,5 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server : {port : 3002}
+  server: {
+    host: true,
+    strictPort: true,
+    port: 8000
+  }
 })
